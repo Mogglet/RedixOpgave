92797c33ad8174bb620f2d3a870da34301e52aca
Mikkel <Mikcollatz@gmail.com>
2026-04-07 11:07:36 +0200

    a

---
+++ RedixOpgave/leaderboard/producer.js
@@ -0,0 +1,17 @@
+﻿const Redis = require('ioredis');
+const redis = new Redis();
+
+setInterval(async () => {
+    const temp = (Math.random() * 10 + 20).toFixed(2);
+
+    await redis.xadd(
+        'temperature-stream',
+        '*',
+        'temperature',
+        temp,
+        'time',
+        new Date().toISOString()
+    );
+
+    console.log('Sent temperature:', temp);
+}, 2000);
---
+++ RedixOpgave/leaderboard/src/stream/stream.service.ts
@@ -0,0 +1,37 @@
+﻿import { Injectable, OnModuleInit } from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class StreamService implements OnModuleInit {
+    private redis: Redis;
+
+    onModuleInit() {
+        this.redis = new Redis(); 
+        this.listenToStream();
+    }
+
+    async listenToStream() {
+        let lastId = '$'; 
+
+        while (true) {
+            const response = await this.redis.xread(
+                'BLOCK',
+                0,
+                'STREAMS',
+                'temperature-stream',
+                lastId
+            );
+
+            if (response) {
+                const [_, messages] = response[0];
+
+                for (const message of messages) {
+                    const [id, data] = message;
+                    lastId = id;
+
+                    console.log('Temperature event:', data);
+                }
+            }
+        }
+    }
+}
